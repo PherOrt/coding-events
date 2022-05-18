c64e36c944a61e878840795e55f5e8cfb4bb7001
COrtega <christopheraortega@gmail.com>
2022-05-18 15:13:31 -0500

    exercises

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
@@ -17,8 +18,12 @@ public class EventController {
 
     private static List<String> events = new ArrayList<>();
 
-    @GetMapping
+    @GetMapping("index")
     public String displayAllEvents(Model model){
+        HashMap<String, String> events =new HashMap<>();
+        events.put("Menteaship","A fun meetup for connecting with mentors");
+        events.put("Code With Pride","A fun meetup sponsored by LaunchCode");
+        events.put("Javascripty", "An imaginary meetup for Javascript developers");
         model.addAttribute("events", events);
         return "events/index";
     }
